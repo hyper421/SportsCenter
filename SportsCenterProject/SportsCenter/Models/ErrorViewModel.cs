ca045388b227f87827d03532945ef148c71566d8
Jena Lin <jenalinp81@gmail.com>
2022-10-09 16:05:02 +0800

    no message

--- SportsCenterProject/SportsCenter/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SportsCenter.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
