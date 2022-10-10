bc5ef5c51d42a1dbeb5c437d1a0de0a6cbc47a6f
David <david8740lie@gmail.com>
2022-10-10 23:34:11 +0800

    註冊控制器

---
+++ SportsCenter/Controllers/RegisterController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsCenter.Models.Hashing;
+
+namespace SportsCenter.Controllers
+{
+    public class RegisterController : Controller
+    {
+        HashingPassword hashingPassword = new HashingPassword();
+        public IActionResult Signin()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Signin([Bind("MemberName,MemberAccount,MemberPassword,MemberAddress,MemberCellphone,MemberEmail")] Member member)
+        {
+            if (ModelState.IsValid)
+            {
+                //Hash
+                var salt = DateTime.Now.ToString();
+                member.MemberSalt = salt;
+                var password = hashingPassword.HashPassword($"{member.MemberPassword}{member.MemberSalt}");
+                member.MemberPassword = password;
+
+
+
+                //等待連結資料庫
+                //_mVCProjectContext.Add(member);
+                //await _mVCProjectContext.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
