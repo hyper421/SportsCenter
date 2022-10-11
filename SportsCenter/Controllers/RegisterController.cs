cd1b0dfb0ef2869524a14bc444d0a35450f0f609
David <david8740lie@gmail.com>
2022-10-11 14:06:18 +0800

    修正連線字串

--- SportsCenter/Controllers/RegisterController.cs
+++ SportsCenter/Controllers/RegisterController.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsCenter.Models.Hashing;
+using SportsCenter.Models.Table;
 
 namespace SportsCenter.Controllers
 {
     public class RegisterController : Controller
     {
         HashingPassword hashingPassword = new HashingPassword();
+        private readonly SportsCenterDbContext _context;
+        public RegisterController(SportsCenterDbContext mVCProjectContext)
+        {
+            this._context = mVCProjectContext;
+        }
         public IActionResult Signin()
         {
             return View();
@@ -18,15 +24,15 @@ namespace SportsCenter.Controllers
             {
                 //Hash
                 var salt = DateTime.Now.ToString();
-                member.MemberSalt = salt;
-                var password = hashingPassword.HashPassword($"{member.MemberPassword}{member.MemberSalt}");
-                member.MemberPassword = password;
+                member.Member_Salt = salt;
+                var password = hashingPassword.HashPassword($"{member.Member_Password}{member.Member_Salt}");
+                member.Member_Password = password;
 
 
 
                 //等待連結資料庫
-                //_mVCProjectContext.Add(member);
-                //await _mVCProjectContext.SaveChangesAsync();
+                _context.Add(member);
+                await _context.SaveChangesAsync();
                 return RedirectToAction("Index", "Home");
             }
             return View();
