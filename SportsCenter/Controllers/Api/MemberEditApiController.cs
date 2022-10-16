248b5ddacd93c5a474409236e5265704b38d7d1a
David <david8740lie@gmail.com>
2022-10-16 20:07:18 +0800

    會員資料修改完成

--- SportsCenter/Controllers/Api/MemberEditApiController.cs
+++ SportsCenter/Controllers/Api/MemberEditApiController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using SportsCenter.Models.DavidModel;
 using SportsCenter.Models.Hashing;
 using SportsCenter.Models.Table;
+using System.Diagnostics.Metrics;
+using System.Security.Claims;
 
 namespace SportsCenter.Controllers.Api
 {
@@ -18,10 +21,50 @@ namespace SportsCenter.Controllers.Api
             this._context = SportsCenterDbContext;
         }
         #endregion
+        #region 取會員資料
+        [HttpGet]
+        public IEnumerable<Member> Get()
+        {
+            var id = HttpContext.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Sid).Value;
+            var user = (from a in _context.Member where a.MemberId == int.Parse(id) select a).ToList();
+            return user;
+        }
         [HttpGet("{id}")]
         public Member Get(int id)
         {
             return _context.Member.Find(id);
         }
+        #endregion
+
+
+        #region 回傳會員資料
+        [HttpPost]
+        public bool Post([FromBody] MemberEditModel model)
+        {
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Sid).Value;
+            if (userId == null)
+            {
+                return false;
+            }
+            var user = (from b in _context.Member
+                      where b.MemberId == int.Parse(userId)
+                        select b).FirstOrDefault();
+            if (user == null)
+            {
+                return false;
+            }
+            else
+            {
+                user.Member_Account = model.Member_Account;
+                user.Member_Name = model.Member_Name;
+                user.Member_Phone = model.Member_Phone;
+                user.Member_Email = model.Member_Email;
+                user.Member_Address = model.Member_Address;
+                _context.Update(user);
+            }
+            _context.SaveChanges();
+            return true;
+        }
+        #endregion
     }
 }
--- SportsCenter/Models/DavidModel/MemberEditModel.cs
+++ SportsCenter/Models/DavidModel/MemberEditModel.cs
@@ -6,7 +6,6 @@ namespace SportsCenter.Models.DavidModel
     {
         public string? Member_Name { get; set; }
         public string? Member_Account { get; set; }
-        public string? Member_Password { get; set; }
         public string? Member_Address { get; set; }
         public string? Member_Email { get; set; }
         public string? Member_Phone { get; set; }
