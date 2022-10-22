c7c2a584346e53933c52b3e7482810567fda5d78
Jena Lin <jenalinp81@gmail.com>
2022-10-22 14:30:16 +0800

    LocationController,Index首頁Layout,LocationDetails,_Layout,site.css

--- SportsCenter/Controllers/LocationsController.cs
+++ SportsCenter/Controllers/LocationsController.cs
@@ -14,22 +14,22 @@ namespace SportsCenter.Controllers
         public LocationsController(SportsCenterDbContext context, IHostEnvironment environment)
         {
             _context = context;
-            this.environment = environment;
+            this.environment = environment; //傳照片用
         }
-        // GET: Locations/BackEnd
+        #region 修改Location後台
         public async Task<IActionResult> EditBackEnd()
         {
             return View(await _context.Location.ToListAsync());
         }
-
-
-        // GET: Locations
+        #endregion
+        #region GET Index頁面
+  
         public async Task<IActionResult> Index()
         {
             return View(await _context.Location.ToListAsync());
         }
-
-        // GET: Locations/Details/5
+        #endregion
+        #region 詳細資料頁面
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null || _context.Location == null)
@@ -46,14 +46,14 @@ namespace SportsCenter.Controllers
 
             return View(location);
         }
+        #endregion
 
+        #region Create頁面+照片上傳
         // GET: Locations/Create
         public IActionResult Create()
         {
             return View();
         }
-
-        // POST: Locations/Create
         //要把確切要綁啥寫在Bind
 
         [HttpPost]
@@ -100,59 +100,110 @@ namespace SportsCenter.Controllers
             }
             return RedirectToAction(nameof(Index));
         }
+        #endregion
+        #region Edit頁面+照片上傳 (壞掉要修)
 
-        // GET: Locations/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public IActionResult Edit()
         {
-            if (id == null || _context.Location == null)
-            {
-                return NotFound();
-            }
-
-            var location = await _context.Location.FindAsync(id);
-            if (location == null)
-            {
-                return NotFound();
-            }
-            return View(location);
+            return View();
         }
+        //要把確切要綁啥寫在Bind
 
-        // POST: Locations/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Location_Id,Location_Name,Location_EngName,Location_Area,Location_Address,Location_Phone,Location_Email,Location_ValidFlag")] Location location)
+
+        //public async Task<IActionResult> Create([Bind("Location_Id,Location_Name,Location_EngName,Location_Area,Location_Address,Location_Phone,Location_Email,Location_ValidFlag")] Location location, IFormFile files)
+        public async Task<IActionResult> Edit(LocationViewModel model, IFormFile files)
         {
-            if (id != location.Location_Id)
+
+            if (files == null || files.Length == 0)
             {
-                return NotFound();
+                return Content("沒有選取檔案，請回上一頁");
+                return RedirectToAction("Edit");
             }
 
-            if (ModelState.IsValid)
+            var path = Path.Combine(environment.ContentRootPath, "wwwroot\\Jena\\picture", files.FileName);
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            //    var root = $@"{environment.ContentRootPath}\wwwroot\picture";
+            //var imagePath = root + "\\" + DateTime.Now.ToString("yyyyMMddHHmmss") + files.FileName;
+            //using (var fs = System.IO.File.Create(path))
             {
-                try
-                {
-                    _context.Update(location);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!LocationExists(location.Location_Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                await files.CopyToAsync(fs);
+                fs.Close();
             }
-            return View(location);
+            model.Location.Location_ImageName = files.FileName;
+
+            //if (ModelState.IsValid)
+            if (model != null)
+            {
+                var location = new Location
+                {
+                    Location_Name = model.Location.Location_Name,
+                    Location_EngName = model.Location.Location_EngName,
+                    Location_Area = model.Location.Location_Area,
+                    Location_Address = model.Location.Location_Address,
+                    Location_Phone = model.Location.Location_Phone,
+                    Location_Website = model.Location.Location_Website,
+                    Location_ImageName = model.Location.Location_ImageName,
+
+                };
+                _context.Add(location);
+                await _context.SaveChangesAsync();
+
+            }
+            return RedirectToAction(nameof(Index));
         }
 
-        // GET: Locations/Delete/5
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null || _context.Location == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var location = await _context.Location.FindAsync(id);
+        //    if (location == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(location);
+        //}
+
+        //// POST: Locations/Edit/5
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("Location_Id,Location_Name,Location_EngName,Location_Area,Location_Address,Location_Phone,Location_Email,Location_ValidFlag")] Location location)
+        //{
+        //    if (id != location.Location_Id)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(location);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!LocationExists(location.Location_Id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(location);
+        //}
+        #endregion
+        #region 刪除畫面
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null || _context.Location == null)
@@ -188,7 +239,7 @@ namespace SportsCenter.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
-
+#endregion
         private bool LocationExists(int id)
         {
             return _context.Location.Any(e => e.Location_Id == id);
