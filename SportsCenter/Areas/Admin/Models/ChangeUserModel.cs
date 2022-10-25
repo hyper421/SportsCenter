b2dac0f271ea1080cb5b1ca9ef1186f0ac551b4a
David <david8740lie@gmail.com>
2022-10-25 22:22:44 +0800

    後台USER CRUD API
    前端註冊修正

---
+++ SportsCenter/Areas/Admin/Models/ChangeUserModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Test.Models.DavidModel
+{
+    public class ChangeUserModel
+    {
+        public int Id { get; set; }
+        public int IsActive { get; set; }
+        public int Role { get; set; }
+        public string Name { get; set; }
+        public string Account { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
