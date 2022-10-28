ac1bc12aa4c2411bc76644f7208828c40f192058
reds1210 <reds1210@gmail.com>
2022-10-29 07:03:21 +0800

    類別後台

--- SportsCenter/Areas/Admin/Controllers/CategoryController.cs
+++ SportsCenter/Areas/Admin/Controllers/CategoryController.cs
@@ -1,16 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsCenter.DataAccess;
 
 namespace SportsCenter.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
+        private readonly SportsCenterDbContext _db;
+
+        public CategoryController(SportsCenterDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
         public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
         {
             return View();
         }
--- SportsCenter/Areas/Admin/Models/CategoryCreateDto.cs
+++ SportsCenter/Areas/Admin/Models/CategoryCreateDto.cs
@@ -1,8 +1,9 @@
-﻿namespace SportsCenter.Models.Dto
+﻿namespace SportsCenter.Areas.Admin.Models
 {
     public class CategoryCreateDto
     {
         public string? Name { get; set; }
         public bool IsActive { get; set; }
+        public IFormFile Image { get; set; }
     }
 }
