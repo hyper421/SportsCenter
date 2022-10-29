776125299bac32f40cf5a8f9050356b4b75398ef
David <david8740lie@gmail.com>
2022-10-29 10:32:19 +0800

    商品類別新增

---
+++ SportsCenter/Areas/Admin/Controllers/ItemController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsCenter.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreateItem()
+        {
+            return View();
+        }
+    }
+}
---
+++ SportsCenter/Controllers/Api/ItemApiController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using SportsCenter.Areas.Admin.Models;
+using SportsCenter.DataAccess;
+using SportsCenter.DataAccess.Entity;
+
+namespace SportsCenter.Controllers.Api
+{
+
+    [Route("api/Item/{action}")]
+    [ApiController]
+    public class ItemApiController : ControllerBase
+    {
+        private readonly SportsCenterDbContext context;
+
+        public ItemApiController(SportsCenterDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpPost]
+        public bool CreateItem(CreateItemModel model)
+        {
+            try
+            {
+                context.Category.Add(new DataAccess.Entity.Category
+                {
+                    IsActive = model.IsActive ? 1 : 0,
+                    Name = model.Name,
+
+                });
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
