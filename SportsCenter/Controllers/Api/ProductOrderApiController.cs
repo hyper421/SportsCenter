92f77d1d051656ba3c664ecbb1cd0f410ef7cd9b
David <david8740lie@gmail.com>
2022-10-31 18:42:08 +0800

    商品明細,資料查詢 => 明細邏輯代修正

---
+++ SportsCenter/Controllers/Api/ProductOrderApiController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsCenter.DataAccess;
+
+namespace SportsCenter.Controllers.Api
+{
+    [Route("api/ProductOrder/{action}")]
+    [ApiController]
+    public class ProductOrderApiController : Controller
+    {
+        private readonly SportsCenterDbContext context;
+
+        public ProductOrderApiController(SportsCenterDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public object GetAll()
+        {
+            return context.ProductsOrder.Select(x => new
+            {
+                x.Id,
+                x.OrderDate,
+                x.MemberAddress,
+                x.MemberCellphone,
+                name = (from a in context.Member
+                        where a.Id == x.MemberId
+                        select a.Name).FirstOrDefault()
+            }).ToList();
+        }
+        [Route("{id}")]
+        public object Detail(int id)
+        {
+
+            return context.ProductsOrderDetail.Where(x => x.ProductOrderId == id).Select(x => new
+            {
+                x.ProductName,
+                x.ProductsPrice,
+                x.Count,
+                x.Id
+            });
+        }
+        [HttpDelete]
+        public bool Delete(int id)
+        {
+            try
+            {
+                context.ProductsOrder.Remove(new DataAccess.Entity.ProductsOrder() { Id = id });
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+        }
+        [HttpDelete]
+
+        public bool DeleteDetail(int id)
+        {
+            try
+            {
+                context.ProductsOrderDetail.Remove(new DataAccess.Entity.ProductsOrderDetail() { Id = id });
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+        }
+    }
+}
