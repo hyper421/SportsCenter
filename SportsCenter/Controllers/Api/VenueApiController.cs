0d819e17b3a0e0299f0ac4b4a4179128b53893c0
reds1210 <reds1210@gmail.com>
2022-11-02 17:41:49 +0800

    temp

--- SportsCenter/Controllers/Api/VenueApiController.cs
+++ SportsCenter/Controllers/Api/VenueApiController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SportsCenter.DataAccess;
 using SportsCenter.DataAccess.Entity;
 
@@ -6,34 +7,64 @@ namespace SportsCenter.Controllers.Api
 {
     [Route("api/Venue/{action}")]
     [ApiController]
-        public class VenueApiController: ControllerBase
+    public class VenueApiController : ControllerBase
+    {
+
+        private readonly SportsCenterDbContext _context;
+        public VenueApiController(SportsCenterDbContext _context)
         {
+            this._context = _context;
+        }
+        public object GetVenueByCategory(int categoryId)
+        {
+            var category =  _context.Category.Include(x=>x.LocationBranch).ThenInclude(x=>x.Location).FirstOrDefault(x => x.Id == categoryId);
+            if (category == null) return null;
 
-            private readonly SportsCenterDbContext _context;
-            public VenueApiController(SportsCenterDbContext _context)
+            var temp2 = category.LocationBranch.Select(x => new
             {
-                this._context = _context;
-            }
-            #region  回傳Location資料
-
-            [HttpGet]
-
-            public object Get()
+                x.Name,
+                x.Id,
+                x.Price,
+                x.Memo
+            });
+            var venue = category.LocationBranch.First().Location;
+            var venueObj = new
             {
-                return _context.Location.Select(x=> new {
-                    x.Area,
-                    x.ImagePath,
-                    x.Id,
-                    x.Name,
-                    x.EnglishName,
-                    x.Description,
-                    x.Website,
-                    x.ContactPhone,
-                    x.Address,
-                    categoryIds = x.LocationBranch.Select(x=>x.CategoryId).ToList()
-                });
+                venue.Area,
+                venue.Name,
+                venue.Description,
+                venue.ImagePath,
+                venue.Id,
+                venue.Address
+            };
+            return new
+            {
+                venueObj,
+                temp2
+            };
+        }
 
-            }
+        #region  回傳Location資料
+
+        [HttpGet]
+
+        public object Get()
+        {
+            return _context.Location.Select(x => new
+            {
+                x.Area,
+                x.ImagePath,
+                x.Id,
+                x.Name,
+                x.EnglishName,
+                x.Description,
+                x.Website,
+                x.ContactPhone,
+                x.Address,
+                categoryIds = x.LocationBranch.Select(x => x.CategoryId).ToList()
+            });
+
+        }
 
         #endregion
         #region 回傳詳細資料畫面viaId
@@ -43,15 +74,15 @@ namespace SportsCenter.Controllers.Api
         {
             var data = _context.Location.Where(x => x.Id == id).Select(x => new DataAccess.Entity.Location
             {
-                    Id= x.Id,
-                    Area= x.Area,
-                    ImagePath = x.ImagePath,
-                    Name=x.Name,
-                    EnglishName=x.EnglishName,
-                    Description = x.Description,
-                    Website=x.Website,
-                    ContactPhone=x.ContactPhone,
-                    Address=x.Address,
+                Id = x.Id,
+                Area = x.Area,
+                ImagePath = x.ImagePath,
+                Name = x.Name,
+                EnglishName = x.EnglishName,
+                Description = x.Description,
+                Website = x.Website,
+                ContactPhone = x.ContactPhone,
+                Address = x.Address,
             }).FirstOrDefault();
 
             return Ok(data);
@@ -66,62 +97,40 @@ namespace SportsCenter.Controllers.Api
         #region  CreateLocation資料
 
         [HttpPost]
-            public IActionResult PostLocation([FromBody] Location location)
+        public IActionResult PostLocation([FromBody] Location location)
+        {
+            var i = new Location
             {
-                var i = new Location
-                {
-                    Name = location.Name,
-                    EnglishName = location.EnglishName,
-                    Address = location.Address,
-                    ContactPhone = location.ContactPhone,
-                    Website = location.Website,
-                    ImagePath = location.ImagePath,
-                };
-                _context.Location.Add(i);
-                _context.SaveChanges();
-                return CreatedAtAction(nameof(GetDetail), new { id = location.Id }, location);
-            }
-
-            #endregion
-            #region  刪除
-
-            [HttpDelete("{id}")]
-            public IActionResult Delete(int id)
-            {
-                var location = _context.Location.SingleOrDefault(i => i.Id == id);
-                if (location != null)
-                {
-                    _context.Remove(location);
-                    _context.SaveChanges();
-                    return NoContent();
-                }
-
-                return NotFound();
-            }
-            #endregion
+                Name = location.Name,
+                EnglishName = location.EnglishName,
+                Address = location.Address,
+                ContactPhone = location.ContactPhone,
+                Website = location.Website,
+                ImagePath = location.ImagePath,
+            };
+            _context.Location.Add(i);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetDetail), new { id = location.Id }, location);
         }
+
+        #endregion
+        #region  刪除
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var location = _context.Location.SingleOrDefault(i => i.Id == id);
+            if (location != null)
+            {
+                _context.Remove(location);
+                _context.SaveChanges();
+                return NoContent();
+            }
+
+            return NotFound();
+        }
+        #endregion
     }
-
-
-
-        //[HttpPut("{id}")]
-        //public IActionResult updateLocation(int id, [FromBody] Location update)
-        //{
-        //    var location = _context.Location.SingleOrDefault(i => i.Id == update.Id);
-        //    if (location != null)
-        //    {
-        //        //location.Name = update.Name;
-        //        //location.EnglishName = update.EnglishName;
-        //        //location.Address = update.Address;
-        //        //location.ContactPhone = update.ContactPhone;
-        //        //location.Website = update.Website;
-        //        //location.ImagePath = update.ImagePath;
-        //        location = update;
-        //        _context.Update(location);
-        //        _context.SaveChanges();
-        //        return CreatedAtAction(nameof(GetbyId), new { id = location.Id }, location);
-        //    }
-        //    return BadRequest();
-        //}
+}
 
 
